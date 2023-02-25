e58d1c104bf71d907f2b8159040071e86b24f09c
motsu8 <motsu159357@gmail.com>
2023-02-25 19:16:07 +0900

    クラス化 #9

--- script.js
+++ script.js
@@ -156,42 +156,107 @@ const camera =
         "powerConsumptionWh": 26,
     }]
 ;
-// Step1
-const st1 = document.getElementById("step-1");
-st1.addEventListener('change', (e) => {
-    getModel(e.target.value)
-});
 
-// brandの重複なしリスト
-let brand = new Set();
-for(let i=0; i < camera.length; i++){
-    brand.add(camera[i].brand);
-};
-brand.forEach((e)=>{
-    let selectBrand = document.createElement("option");
-    selectBrand.value = e;
-    selectBrand.innerHTML = e;
-    st1.append(selectBrand);
-});
+// 重複なしBrandリスト
+const noOverLappingBrand = () => {
+    let brand = new Set();
+    for(let i=0; i < camera.length; i++){
+        brand.add(camera[i].brand);
+    };
+    brand.forEach((e)=>{
+        let selectBrand = document.createElement("option");
+        selectBrand.value = e;
+        selectBrand.id = e
+        selectBrand.innerHTML = e;
+        st1.append(selectBrand);
+    });
+    return brand;
+}
 
-// Step2
-const st2 = document.getElementById("step-2");
-function getModel(e){
+// modelリスト
+const getModel = (e) => {
+    st2.innerHTML = "";
     let model = new Set();
     for(let i=0; i<camera.length; i++){
         if(camera[i].brand === e) model.add(camera[i].model)
     }
-
     model.forEach((e)=>{
         let selectModel = document.createElement("option");
         selectModel.value = e;
         selectModel.innerHTML = e;
         st2.append(selectModel);
     })
-    console.log(model);
+    return model;
 }
 
-// step3
+// バッテリー選定
+const getChooseBattery = (model, accessoryPower) => {
+    const calcModelElement = camera.filter(ele => ele.model === model)[0].powerConsumptionWh;
+    console.log(calcModelElement)
+    //const wParHr = voltage * capacityAh;
+    //const h = wParHr / powerConsumptionWh
+    //if(e < maxDraw * endVoltage)
+    //console.log("getPowerConsumption")
+    console.log(accessoryPower)
+    //"capacityAh": 2.3,--
+    //"voltage": 14.4,--
+    //"maxDraw": 3.2,
+    //"endVoltage": 10,
+    /**
+     * テレビの電源電圧が 120V で、電流が 1.2A の場合、
+     * 消費電力は P = I * V を用いて、P = 120(maxDraw) * 1.2(voltage) = 144W となります。
+     * 同様に、電圧が 14.4V で、満充電時電池容量が 6.6Ah（A / hr）の電池は、
+     * 14.4V × 6.6Ah() = 95Wh（W / hr）の電力容量を持ちます。
+     * 仮にその電池を消費電力が 50W のカメラに使用した場合、95 / 50(powerConsumptionWh) ＝ 1.9 時間（114分）
+     * 持続可能であることを意味します。
+    */
+}
+
+// target
+const st1 = document.getElementById("step-1");
+const st2 = document.getElementById("step-2");
 const st3 = document.getElementById("step-3");
-st3.addEventListener('change', (e)=>{console.log(e.target.value)})
+
+// 初期値
+const initial = () => {
+    // brand
+    let brand = noOverLappingBrand()
+    let initBrand = [...brand][2]
+    document.getElementById(initBrand).selected = true
+
+    // model
+    let model = [...getModel(initBrand)]
+    let initModel = model[0];
+
+    // accessory
+    let initAccessory = 50;
+    document.getElementById("step-3").value = initAccessory
+
+    let current = [initBrand, initModel, initAccessory]
+    return current
+}
+
+const current = initial()
+let currentBrand = current[0];
+let currentModel = current[1];
+let currentAccessoryPower = current[2];
+
+// Step1
+st1.addEventListener('change', (e) => {
+    currentBrand = e.target.value;
+    currentModel = [...getModel(currentBrand)][0]
+    getModel(currentBrand)
+});
+
+// Step2
+st2.addEventListener('change', (e)=>{
+    currentModel = e.target.value;
+    getChooseBattery(currentModel, currentAccessoryPower)
+});
+
+// step3
+st3.addEventListener('change', (e)=>{
+    currentPower = e.target.value;
+    getChooseBattery(currentModel, currentAccessoryPower)
+});
 
