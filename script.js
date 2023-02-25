60f56414124eaf10b3ccdfa09c6e9994cf2ab9ea
motsu8 <motsu159357@gmail.com>
2023-02-25 11:05:59 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// initial
+function initial(){
+    const target = document.getElementById("target");
+    target.innerHTML = `
+    <div id="title" class="container-fluid bg-info">Battery Finder Program</div>
+    <div id="input"></div>
+    <div id="output"></div>
+    `
+}
+
+initial();
